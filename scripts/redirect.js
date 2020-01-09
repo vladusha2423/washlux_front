6b4d22c89b3241dbfdf143e8a133d5ca7cbdca0e
Dmitriev Vladislav <dvv2423@ya.ru>
2020-01-09 18:46:28 +0300

    correct numbers

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -4,3 +4,15 @@ $('.nav-logo').click(function(){
 $('.nav-phone').click(function(){
     document.location.href = "tel:+78123098111"
 });
+$('.call-chkal').click(function(){
+    document.location.href = "tel:+79214111100"
+});
+$('.call-lit').click(function(){
+    document.location.href = "tel:+79118211100"
+});
+$('.call-10line').click(function(){
+    document.location.href = "tel:+79312571100"
+});
+$('.call-17line').click(function(){
+    document.location.href = "tel:+79312101100"
+});
